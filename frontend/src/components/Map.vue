367b99574e13982b83782085363facaf31a3f204
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-05-18 18:39:35 -0500

    add basic Map component

---
+++ frontend/src/components/Map.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="map" style="height:100%; width:100%">
+    <l-map
+      style="height: 100%; width: 100%"
+      :zoom="zoom"
+      :center="center"
+      @update:zoom="zoomUpdated"
+      @update:center="centerUpdated"
+      @update:bounds="boundsUpdated"
+    >
+      <l-tile-layer :url="url"></l-tile-layer>
+    </l-map>
+    </div>
+</template>
+
+<script>
+import { LMap, LTileLayer } from 'vue2-leaflet'
+
+export default {
+  components:{
+    LMap,
+    LTileLayer
+  },
+  data () {
+    return {
+      url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+      zoom: 3,
+      center: [47.413220, -1.219482],
+      bounds: null
+    };
+  },
+  methods: {
+    zoomUpdated (zoom) {
+      this.zoom = zoom;
+    },
+    centerUpdated (center) {
+      this.center = center;
+    },
+    boundsUpdated (bounds) {
+      this.bounds = bounds;
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.map {
+  * {
+    z-index: 0;
+  }
+}
+</style>
