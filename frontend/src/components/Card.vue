69958e660ff0fc7c3b988d83add5288352bed74c
Rebecca V <rebeccarvickers@gmail.com>
2019-05-18 19:36:45 -0500

    added icon files for card component

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -6,7 +6,7 @@
           src="https://assets.simpleviewinc.com/simpleview/image/fetch/c_limit,h_640,q_60,w_800/https://assets.simpleviewinc.com/simpleview/image/upload/crm/houston/Niko-Niko-1_3ce738f4-fdbc-f6b6-bdb3824cf1b77b11.jpg"
           aspect-ratio="2.75"
         ></v-img>
-        <v-card-title primary-title>Niko Niko's [icon] [icon] [icon]
+        <v-card-title primary-title>Niko Niko's <img src="../assets/condiments.png"> <img src="../assets/containers.png"> <img src="../assets/straws.png">
         </v-card-title>
         <v-card-title secondary-title>123 Smith St., Houston, TX.</v-card-title>
         <v-card-text>
@@ -23,3 +23,10 @@ export default {
 
 }
 </script>
+
+<style>
+  img {
+    width: 1.8em;
+    margin: 0 .5em .8em .5em;
+  }
+</style>
