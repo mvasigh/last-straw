14978a88f5a0cbf6ea4e9b23db97de998ba91271
RCH <rchillard@gmail.com>
2019-05-18 22:05:18 -0500

    Introduce Model for Places and seedDatabase function

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const Sequelize = require('sequelize');
 
+// SERVER SETUP
 // Instatiate the Express app
 const app = express();
 
@@ -12,16 +13,69 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// DATABASE
 // Connecting to the Database
 // Development
 const sequelize = new Sequelize(
   'postgres://rdeboeor:apSXDd2s_Cu3frmThQCmWJIjvjCrg9Qs@isilo.db.elephantsql.com:5432/rdeboeor'
 );
 
+// Defining Models for Database
+const Place = sequelize.define('place', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  address: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lat: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  long: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  styrofoam: {
+    type: Sequelize.BOOLEAN,
+    allowNull: true
+  },
+  plastic: {
+    type: Sequelize.BOOLEAN,
+    allowNull: true
+  },
+  icondiments: {
+    type: Sequelize.BOOLEAN,
+    allowNull: true
+  },
+  compostable: {
+    type: Sequelize.BOOLEAN,
+    allowNull: true
+  }
+});
+
+// Function to add random data to database
+const seedDatabase = async () => {
+  await Place.create({
+    name: 'Merida Mexican Restaurant',
+    address: '2509 Navigation Blvd, Houston, TX 77003',
+    lat: 29.7582547,
+    long: 95.34373,
+    styrofoam: true,
+    plastic: false,
+    icondiments: true,
+    compostable: false
+  });
+};
+
+// ROUTES
 app.get('/', (req, res) => {
   res.send('hello world');
 });
 
+// Test connection with remote SQL database
 sequelize
   .authenticate()
   .then(() => {
@@ -31,7 +85,17 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 
+// Easily setup/seed database
+const eraseDatabaseOnSync = true;
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+
+// Synchronize database and start Express server
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    seedDatabase();
+  }
+
+  app.listen(port, () =>
+    console.log(`Destroyer of straws server listening on port ${port}!`)
+  );
 });
