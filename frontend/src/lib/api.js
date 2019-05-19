9ea21cc9cb3ad0dafd350856bb6a442358817b46
Mehdi Vasigh <mehdi.vasigh@arundo.com>
2019-05-19 09:53:09 -0500

    add getPlace function

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,11 +1,14 @@
 import axiosRaw from 'axios';
 
-let API_URL = process.env.VUE_APP_API_URL
+let API_URL = process.env.VUE_APP_API_URL;
 const axios = axiosRaw.create({
   baseURL: API_URL
 });
 
-export async function getAllPlaces() {
-  const places = axios.get('places').then(({ data }) => data);
-  return places;
+export function getAllPlaces() {
+  return axios.get('places').then(({ data }) => data);
+}
+
+export async function getPlace(placeId) {
+  return axios.get(`places/${placeId}`).then(({ data }) => data);
 }
