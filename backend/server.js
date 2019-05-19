2ed46fc1afa7568497383b47e648a183ba99d401
RCH <rchillard@gmail.com>
2019-05-18 21:29:11 -0500

    Add basic Sequelize and connection test

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,37 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Sequelize = require('sequelize');
 
+// Instatiate the Express app
 const app = express();
 
-// middleware
+// Middleware for Express
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Connecting to the Database
+// Development
+const sequelize = new Sequelize(
+  'postgres://rdeboeor:apSXDd2s_Cu3frmThQCmWJIjvjCrg9Qs@isilo.db.elephantsql.com:5432/rdeboeor'
+);
+
 app.get('/', (req, res) => {
   res.send('hello world');
-})
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
